d9394ff6a00e3f8350acb39fe92e5f747e80f02d
nilesh-surendran <91998316+nilesh-surendran@users.noreply.github.com>
2021-12-02 21:30:57 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,48 @@
+var ball;
+var database, position 
+
+function setup(){
+    database = firebase.database()
+    createCanvas(500,500);
+    ball = createSprite(250,250,10,10);
+    ball.shapeColor = "red";
+    var bPos = database.ref("ball/position")
+    bPos.on("value",readPos,showError)
+
+
+}
+
+function draw(){
+    background("white");
+    if(keyDown(LEFT_ARROW)){
+        writePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        writePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        writePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        writePosition(0,+1);
+    }
+    drawSprites();
+}
+
+function readPos(data){
+    position = data.val()
+    ball.x = position.x
+    ball.y = position.y
+}
+
+function showError(){
+    console.log("dataNotRecivedFromTheDatabase")
+}
+
+function writePosition(x,y){
+    database.ref("ball/position").set({
+        x: position.x+x, 
+        y: position.y+y
+    })
+
+}
